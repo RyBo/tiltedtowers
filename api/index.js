426aeaa18314804536dbf67ce6c9493b0933f492
Ryan Bohannon <ryan.m.bohannon@gmail.com>
2018-04-26 18:01:08 -0600

    Added login,signup,sessions and mysql to application for class

---
+++ api/index.js
@@ -0,0 +1,116 @@
+//
+const qs = require('qs');
+const axios = require('axios');
+const auth = require('../auth');
+
+var authtoken = ['',''];
+
+// Helper function for getting Spotify's auth token
+exports.getAuth = function() {
+   
+    var nowTime = new Date();
+    var expireTime = authtoken[1];
+
+    if (nowTime < expireTime) {
+        return;
+    } else {
+        console.log("Spotify auth token expired, fetching a new one");
+    }
+
+    var expireTime = new Date();
+    // API token lasts an hour, set expire time to one hour from now
+    expireTime.setHours(expireTime.getHours()+1);
+
+    var token = ''; 
+    const headers = {'Content-Type' : 'application/x-www-form-urlencoded',
+        'Authorization' : auth.spotify};
+
+    const data = qs.stringify({'grant_type' : 'client_credentials'});
+
+    return new Promise((resolve, reject) => { 
+        axios.post('https://accounts.spotify.com/api/token', data, {headers: headers})
+            .then((response) => {
+                token = response.data['access_token'];
+                authtoken[0] = token;
+                authtoken[1] = expireTime;
+                console.log(authtoken);
+                return resolve(token);
+            })
+            .catch((error) => {
+                console.log(error);
+                return reject(error);
+            })  
+    }); 
+};
+
+// Get playlists from spotify's API
+//app.get('/api/spotify/playlists', (req, res) => {
+exports.playlists = function(req, res) {
+
+//    getAuth();
+
+    const headers = {'Content-Type' : 'application/x-www-form-urlencoded','Authorization' : 'Bearer ' + authtoken[0] };
+
+    axios.get('https://api.spotify.com/v1/users/rabidowl/playlists?limit=50', {headers: headers})
+        .then((response) => {
+            res.json(response.data);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+};
+
+// Get tracks from selected playlist
+//app.get('/api/spotify/users/:user/playlists/:playlist/tracks', (req, res) => {
+exports.tracks = function(req, res) {
+
+ //   getAuth();
+
+    const user = req.params.user;
+    const playlist = req.params.playlist;
+
+    const headers = {'Content-Type' : 'application/x-www-form-urlencoded','Authorization' : 'Bearer ' + authtoken[0] };
+    axios.get('https://api.spotify.com/v1/users/'+user+'/playlists/'+playlist+'/tracks', {headers: headers})
+        .then((response) => {
+            res.json(response.data);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+};
+
+
+// Youtube API Access
+//app.post('/api/youtube', (req, res) => {
+exports.youtube = function(req, res) {
+
+    const instrument = req.body.instrument;
+    const artist = req.body.artist;
+    const song = req.body.song;
+    const searchParams = "search?q="+artist+" "+ song + " " + instrument + "+lesson+tutorial&part=snippet&maxResults=3&key=" + auth.youtube;
+
+    axios.get('https://www.googleapis.com/youtube/v3/' + searchParams)
+        .then((response) => {
+            res.json(response.data);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+};
+
+// Spotify Search Feature
+//app.get('/api/spotify/search/:query', (req, res) => {
+exports.spotifySearch = function(req, res) {
+
+    const query = req.params.query;
+    const searchParams = 'search?q='+query+'&type=playlist,album,artist&limit=3';
+
+    axios.get('https://api.spotify.com/v1/' + searchParams)
+        .then((response) => {
+            res.json(response.data);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+};
+
--- server.js
+++ server.js
@@ -1,133 +1,73 @@
-const express = require('express');
-const path = require('path');
-const https = require('https');
-const axios = require('axios');
-const qs = require('qs');
-const moment = require('moment');
-const bodyParser = require('body-parser');
+const express = require('express')
+    , routes = require('./routes')
+    , api = require('./api')
+    , user = require('./routes/user')
+    , http = require('http') 
+    , path = require('path');
 
+const bodyParser = require('body-parser');
+const session = require('express-session');
 const auth = require('./auth');
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host : auth.mysql_host,
+    user : auth.mysql_user,
+    password : auth.mysql_password,
+    database : auth.mysql_database
+});
+connection.connect();
+global.db = connection;
 
 const app = express();
+// Session Configuration
+app.use(session({
+    secret: 'chefpieter',
+    resave: false,
+    saveUninitialized: true,
+    cookie : { maxAge: 6000}
+}));
 
-var authtoken = ['',''];
-getAuth();
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+// ENV Configuration
+app.set('port', process.env.PORT || 5000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-// Helper function for getting Spotify's auth token
-function getAuth() {
-   
-    var nowTime = new Date();
-    var expireTime = authtoken[1];
+//app.get('/', routes.index);
 
-    if (nowTime < expireTime) {
+var authtoken = ['',''];
+api.getAuth();
+
+
+// Load the REACT App after user logs in
+app.get('/', (req, res) => {
+    var user = req.session.user;
+    userId = req.session.userId;
+
+    if (userId == null) {
+        res.redirect('/login')
         return;
-    } else {
-        console.log("Spotify auth token expired, fetching a new one");
     }
-
-    var expireTime = new Date();
-    // API token lasts an hour, set expire time to one hour from now
-    expireTime.setHours(expireTime.getHours()+1);
-
-    var token = '';
-    const headers = {'Content-Type' : 'application/x-www-form-urlencoded',
-        'Authorization' : auth.spotify};
-
-    const data = qs.stringify({'grant_type' : 'client_credentials'});
-
-    return new Promise((resolve, reject) => { 
-        axios.post('https://accounts.spotify.com/api/token', data, {headers: headers})
-            .then((response) => {
-                token = response.data['access_token'];
-                authtoken[0] = token;
-                authtoken[1] = expireTime;
-                console.log(authtoken);
-                return resolve(token);
-            })
-            .catch((error) => {
-                console.log(error);
-                return reject(error);
-            })
-    });
-}   
-
-// Get playlists from spotify's API
-app.get('/api/spotify/playlists', (req, res) => {
-
-    getAuth();
-
-    const headers = {'Content-Type' : 'application/x-www-form-urlencoded','Authorization' : 'Bearer ' + authtoken[0] };
-
-    axios.get('https://api.spotify.com/v1/users/rabidowl/playlists?limit=50', {headers: headers})
-        .then((response) => {
-            res.json(response.data);
-        })
-        .catch((error) => {
-            console.log(error);
-        })
+    res.sendFile(path.join(__dirname+'/client/build/app.html'));
 });
 
-// Get tracks from selected playlist
-app.get('/api/spotify/users/:user/playlists/:playlist/tracks', (req, res) => {
+// Extra Routes
+app.get('/login', routes.index);
+app.post('/login', user.login);
+app.get('/signup', user.signup);
+app.post('/signup', user.signup);
 
-    getAuth();
-
-    const user = req.params.user;
-    const playlist = req.params.playlist; 
-
-    const headers = {'Content-Type' : 'application/x-www-form-urlencoded','Authorization' : 'Bearer ' + authtoken[0] };
-    axios.get('https://api.spotify.com/v1/users/'+user+'/playlists/'+playlist+'/tracks', {headers: headers})
-        .then((response) => {
-            res.json(response.data);
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-});
-
-// Youtube API Access
-app.post('/api/youtube', (req, res) => {
-
-    const instrument = req.body.instrument;
-    const artist = req.body.artist;
-    const song = req.body.song;
-    const searchParams = "search?q="+artist+" "+ song + " " + instrument + "+lesson+tutorial&part=snippet&maxResults=3&key=" + auth.youtube;
-
-    axios.get('https://www.googleapis.com/youtube/v3/' + searchParams)
-        .then((response) => {
-            res.json(response.data); 
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-});
-
-// Spotify Search Feature
-app.get('/api/spotify/search/:query', (req, res) => {
-
-    const query = req.params.query;
-    const searchParams = 'search?q='+query+'&type=playlist,album,artist&limit=3';
-    
-    axios.get('https://api.spotify.com/v1/' + searchParams)
-        .then((response) => {
-            res.json(response.data);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-});
+app.get('/api/spotify/playlists', api.playlists);
+app.get('/api/spotify/users/:user/playlists/:playlist/tracks', api.tracks);
+app.post('/api/youtube', api.youtube);
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
+//app.get('*', (req, res) => {
 
-const port = process.env.PORT || 5000;
-app.listen(port);
-console.log(`Backend API listening on ${port}`);
+app.listen(5000);
+console.log(`Backend API listening on port 5000`);
