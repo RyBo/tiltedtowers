cde76c103e1da47178de562a454ce4d2842d196b
Ryan Bohannon <ryan.m.bohannon@gmail.com>
2018-04-26 18:10:53 -0600

    small fix to sslconfig.js file

--- sslconfig.js
+++ sslconfig.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
-const privateKey = fs.readFileSync('sslcert/server.key');
-const certificate = fs.readFileSync('sslcert/server.crt');
+const privateKey = fs.readFileSync('sslcert/server.key', 'utf8');
+const certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
 
 module.exports = {
-    credentials : {key: privateKey, cert: certificate};
+    credentials : {key: privateKey, cert: certificate}
 }
